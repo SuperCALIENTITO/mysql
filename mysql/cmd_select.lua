9282f4f6f46104a495948b34fe84fe7fff1e423a
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-16 19:13:35 -0300

    Update cmd_select.lua

--- mysql/cmd_select.lua
+++ mysql/cmd_select.lua
@@ -15,7 +15,7 @@ Los datos que se devuelven/entregan se almacenan en una tabla de resultados, den
         input = "SELECT * FROM Clientes;",
         output = {
             ["head"] = {"ID",       "Nombre",       "Apellido",     "Pais" },
-            [1] = {     "1",        "Arman",        "Luis",         "Rberto" },
+            [1] = {     "1",        "Arman",        "Luis",         "Roberto" },
             [2] = {     "2",        "Vicente",      "Salas",        "Torres" },
             [3] = {     "3",        "Argentina",    "Bolivia",      "Chile" }
         }
