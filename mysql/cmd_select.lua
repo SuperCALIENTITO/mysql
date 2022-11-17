1d99ee750743e6e390e2be2e93086fbab8e9be8a
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-17 12:50:45 -0300

    commit

--- mysql/cmd_select.lua
+++ mysql/cmd_select.lua
@@ -9,9 +9,18 @@ Los datos que se devuelven/entregan se almacenan en una tabla de resultados, den
     },
 
     [2] = {
+        type = "Example",
+        title = "Ejemplos de Escritura",
+        content = {
+            [1] = { "SELECT <i>columna1</i>, <i>columna2</i>, ...<br>FROM tabla_datos;", [[Selecciona las columnas <a class="example">columna1</a> y <a class="example">columna2</a> de datos de la tabla <a class="example">tabla_datos</a>]] },
+            [2] = { "SELECT * FROM tabla;", [[Selecciona todas las columnas dentro de la tabla <a class="example">tabla_datos</a>]]}
+        }
+    },
+
+    [3] = {
         type = "Table",
         title = "Ejemplos",
-        description = "Testing",
+        description = [[Con el comando <a class="example">SELECT</a> se puede seleccionar todos los datos de una tabla de la siguiente forma.]],
         input = "SELECT * FROM Clientes;",
         output = {
             ["head"] = {"ID",       "Nombre",       "Apellido",     "Pais" },
